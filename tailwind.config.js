7ba450bfe96760004e6aab58463becc7eadc8336
Alexander Acojido <alexacojido@gmail.com>
2024-01-28 16:04:20 +0800

    updated ui

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'Primary': 'hsl(47, 88%, 63%)',
+        'White': 'hsl(0, 0%, 100%)',
+        'Grey': 'hsl(0, 0%, 50%)',
+        'Black': 'hsl(0, 0%, 7%)'
+      },
+      fontFamily: {
+        figtree: ['Figtree'],
+        sansserif:['sans-serif'],
+        youngserif600: ['Figtree', 'sans-serif', '600'],
+        youngserif800: ['Figtree', 'sans-serif', '800']
+      },
+    },
+  },
+  plugins: [],
+}
+
